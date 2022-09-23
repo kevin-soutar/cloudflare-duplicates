3ccaac112919f015b122b9ccc03f7d3fd5ee8928
Kevin Soutar <60884768+kevin-soutar@users.noreply.github.com>
2022-09-22 23:53:52 -0400

    New Cloudflare Zone Script
    Updated script to pull domain zones from Cloudflare dynamically based on account

--- Cloudflare_detect_duplicates.py
+++ Cloudflare_detect_duplicates.py
@@ -1,27 +1,44 @@
 import requests
 import secretinfo
 
-def cloudflare_records(page_num,per_page=100,direction='asc'):
-    cloudflare_headers = {
-    'X-Auth-Email': secretinfo.cloudflare_email ,
-    'Authorization': secretinfo.cloudflare_api_key,
-    'Content-Type': 'application/json',
-    }
+cloudflare_headers = {
+'X-Auth-Email': secretinfo.cloudflare_email ,
+'Authorization': secretinfo.cloudflare_api_key,
+'Content-Type': 'application/json',
+}
 
-    response = requests.get(f"https://api.cloudflare.com/client/v4/zones/{secretinfo.cloudflare_zone_id}/dns_records?page={page_num}&per_page={per_page}&order=type&direction={direction}", headers=cloudflare_headers)
+def cloudflare_zones(page_num,per_page=100,direction='asc'):
+    response = requests.get(f"https://api.cloudflare.com/client/v4/zones?page={page_num}&per_page={per_page}&order=type&direction={direction}", headers=cloudflare_headers)
     return response.json()
 
-page_num =1
-original_output =["No Result"]
+def cloudflare_records(page_num,zone_id,per_page=100,direction='asc'):
+    response = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records?page={page_num}&per_page={per_page}&order=type&direction={direction}", headers=cloudflare_headers)
+    return response.json()
+
+
+page_num = 1
+original_zone =["No Result"]
+zone_ids = {}
+
+while original_zone != [] or None:
+    output = cloudflare_zones(page_num)
+    page_num = page_num + 1
+    original_zone = output['result']
+    for domain in output['result']:
+        zone_ids.setdefault(domain['id'], set()).add(domain['name'])
+
 a_records = {}
 
-while original_output != []:
-    output = cloudflare_records(page_num)
-    page_num = page_num + 1
-    original_output = output['result']
-    for id, record in enumerate(output["result"]):
-        if record["type"] == "A":
-            a_records.setdefault(record["content"], set()).add(record["name"])
+for zone_id in zone_ids:
+    page_num =1
+    original_output =["No Result"]
+    while original_output != []:
+        output = cloudflare_records(page_num,zone_id=zone_id)
+        page_num = page_num + 1
+        original_output = output['result']
+        for id, record in enumerate(output["result"]):
+            if record["type"] == "A":
+                a_records.setdefault(record["content"], set()).add(record["name"])
 
 results = filter(lambda x: len(x) >1, a_records.values())
 
--- secretinfo.py
+++ secretinfo.py
@@ -1,6 +1,5 @@
 cloudflare_email = ''
 cloudflare_api_key  = ''
-cloudflare_zone_id = ''
 
 postmark_server_token = ''
 postmark_send_email = ''
